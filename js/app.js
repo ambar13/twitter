7c24c589aa024ab1831e7c855c131a2df10aede0
ambar13 <ambar13_9@hotmail.com>
2017-11-20 05:19:53 -0500

    modificando js

--- js/app.js
+++ js/app.js
@@ -1,67 +1,38 @@
 var textarea = document.getElementsByTagName('textarea');
 var btn = document.getElementsById('btn');
 var contador = document.getElementsByClassName('cont');
-document.getElementById('btn').disabled = true;
 
-btn.addEventListener('click', activeBtn);
-function activeBtn (event){
-  textarea.value;
-  var box = document.createElement('div');t
-  box.textContent = textarea;
-  x = document.querySelector('#twettBox');
-  x.appendChild(box);
+textarea.addEventListener('keyup', value);
+validar();
+textarea.addEventListener('keyup', countString);
+textarea.addEventListener('keydown', textareaAutosize);
+
+
+btn.addEventListener('click', sendTweet);
+function sendTweet (event){
+  var valueTextArea = textarea.value;
+  var publicc = document.createElement('div');
+  publicc.textContent = valueTextArea;
+  document.querySelector('#twettBox').appendChild(publiccs);
+  document.querySelector('.textarea').value = '';
 }
  
 
 
+function valueBtn(){
+	  if(document.querySelector('.textarea').value === ''){
+	    btn.disabled = true;
+	  } else {
+	    btn.disabled = false;
+	  }
+	}
 
-
-
-
-txtarea.addEventListener('keyup', textsize);
-function textsize(event) {
-  textTweet.style.height = '200px';
-  textTweet.style.height = textTweet.scrollHeight + 'px';
+// textarea agranda
+function textareaAutosize() {
+	  var box = this;
+	  setTimeout(function() {
+	    box.style.cssText = 'height: auto; padding: 0';
+	    box.style.cssText = 'height:' + box.scrollHeight + 'px';
+	  },0);
+	}
 }
-
-
-var area = document.getElementById('insert-links');
-var boton = document.getElementById('save');
-var lista = document.getElementById('container-links');
-
-boton.addEventListener('click', function(event){
-  if(area.value){
-    var li = document.createElement('li');
-    var link = document.createElement('div');
-
-    link.textContent = area.value;
-    link.setAttribute.style.heigtht = "100px";
-
-    li.appendChild(link);
-
-    lista.appendChild(li);
-
-    area.value = '';
-  }
-});
-
-
-
-
-// txtarea.addEventListener('keyup', text);
-  
-
-//   function text(event) {
-//   	textarea.style.heigth = '200px';
-//   }
-// }
-// var textarea = document.querySelector('textarea');
-// textarea.addEventListener('keydown', autosize);
-             
-// function autosize(){
-//   var el = this;
-//   setTimeout(function(){
-//     el.style.cssText = 'height:auto; padding:0';
-//     el.style.cssText = 'height:' + el.scrollHeight + 'px';
-//   },0);
-// }
